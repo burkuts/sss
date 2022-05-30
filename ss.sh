d0be571eb6db91008bf98ede23fb022d33910ca5
burkuts <83317998+burkuts@users.noreply.github.com>
2022-05-30 19:04:59 +0300

    Delete ss.sh

--- ss.sh
+++
@@ -1,7 +0,0 @@
-@echo OFF
-REM Change the following address to your BEAM addr.
-SET ADDRESS=e672b0582c85bd54e6f62e3be5a983fd297a80328ae550889c68a7cc836ca6a26a
-SET USERNAME=%ADDRESS%.w
-SET POOL=beam.sparkpool.com:2222
-SET SCHEME=beamhash3+ssl
-START "Bminer: When Crypto-mining Made Fast" bminer.exe -uri %SCHEME%://%USERNAME%@%POOL% -api 127.0.0.1:1880
